88276bd91f9a2b37c15e51f6c4bed4d1b12b3cb0
Ehaabsyed <syedehaab12@gmail.com>
2025-09-22 22:09:55 +0530

    Ready for deploy

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -4,10 +4,10 @@ import { Link, useNavigate } from "react-router-dom";
 function Home() {
   const navigate=useNavigate();
   return (
-    <div className="min-h-screen w-full flex flex-col items-center justify-center bg-gray-100">
+    <div className="min-h-screen overflow-hidden w-full flex flex-col items-center justify-center bg-gray-100">
       <div className="navbar absolute top-0 left-0 w-full h-16 flex items-center justify-between p-4 bg-black bg-opacity-50">
         <h1 className="text-3xl flex justify-center items-center font-extrabold z-20 text-white gap-2.5">PrismCode <FaCodepen className="z-20 text-3xl text-white" /></h1>
-        {/* <button className="z-20 px-3 py-1 hover:scale-109 animate ease-in-out transition cursor-pointer duration-150 bg-white text-black rounded-full border-none font-bold mt-3">Login</button> */}
+        <button className="z-20 px-3 py-1 hover:scale-109 animate ease-in-out transition cursor-pointer duration-150 bg-white text-black rounded-full border-none font-bold mt-3">Login</button>
 
       </div>
       <img src="/bg.jpg" className="h-full absolute top-0 left-0 w-full object-cover" alt="" />
